016512bbdff7d2dd34fbec18ee872d7e804d6f3b
Nisala - Chamodya <113490575+Nisala-Chamodya@users.noreply.github.com>
2024-05-14 13:49:10 +0530

    Setup Project

---
+++ zoory-backers-font/src/App.jsx
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <h1 className="bg-orange font-primary">Hello Developer</h1>;
+}
+
+export default App;
---
+++ zoory-backers-font/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        orange: "#FF9800",
+        red: "#FF6868",
+        secondary: "#555",
+        primaryBG: "#FCFCFC",
+      },
+    },
+  },
+  plugins: [],
+};
+/*   fontFamily: {
+        primary: ["Inter", "sans-serif"],
+      }, */
