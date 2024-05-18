3194a1255343125b1b2cdc5911f70911ff1618fc
Nisala - Chamodya <113490575+Nisala-Chamodya@users.noreply.github.com>
2024-05-18 19:53:32 +0530

    Instrall React Slick

--- zoory-backers-font/src/pages/home/Home.jsx
+++ zoory-backers-font/src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../../component/Banner";
 import Categories from "../../component/Categories";
+import SpecialDishes from "../../component/SpecialDishes";
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <Categories />
+      <SpecialDishes />
     </div>
   );
 };
