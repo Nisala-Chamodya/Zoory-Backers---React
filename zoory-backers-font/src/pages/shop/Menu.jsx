3aab63fb94d0474957c82f5102a38ce65b2ec746
Nisala - Chamodya <113490575+Nisala-Chamodya@users.noreply.github.com>
2024-05-23 07:30:30 +0530

    fetch data from menu.json

--- zoory-backers-font/src/pages/shop/Menu.jsx
+++ zoory-backers-font/src/pages/shop/Menu.jsx
@@ -1,6 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Menu = () => {
+  const [menu, setMenu] = useState([]);
+  const [filterdItems, setFilterdItems] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("all");
+  const [sortOption, setSortOption] = useState("default");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(8);
+
+  {
+    /*start loading data*/
+  }
+  useEffect(() => {
+    {
+      /*start fetch data from the back end*/
+    }
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/menu.json");
+        const data = await response.json();
+        //console.log(data);
+        setMenu(data);
+        setFilterdItems(data);
+      } catch (error) {
+        console.log("Error Fetching Data", error);
+      }
+    };
+    {
+      /*end fetch data from the back end*/
+    }
+    //call the function
+    fetchData();
+  }, []);
+  {
+    /*end loading data*/
+  }
+
   return (
     <div className="mt-10">
       {/*start menu banner */}
@@ -23,8 +58,11 @@ const Menu = () => {
           {/*end text*/}
         </div>
       </div>
-
       {/*end menu banner */}
+
+      {/*start menu shop section */}
+      <div className="section-container"></div>
+      {/*end menu shop section */}
     </div>
   );
 };
