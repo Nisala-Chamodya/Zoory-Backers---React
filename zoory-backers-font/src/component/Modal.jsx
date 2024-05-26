b10f8ce5d1eb45c0594e8cb87c600874c9b4e59f
Nisala - Chamodya <113490575+Nisala-Chamodya@users.noreply.github.com>
2024-05-26 16:09:57 +0530

    Customixze Modal

--- zoory-backers-font/src/component/Modal.jsx
+++ zoory-backers-font/src/component/Modal.jsx
@@ -1,16 +1,89 @@
 import React from "react";
+import { FaFacebookF, FaGithub, FaGoogle } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Modal = () => {
   return (
     <dialog id="my_modal_5" className="modal modal-middle sm:modal-middle">
       <div className="modal-box">
-        <h3 className="text-lg font-bold">Hello!</h3>
-        <p className="py-4">Press ESC key or click the button below to close</p>
-        <div className="modal-action">
-          <form method="dialog">
-            {/* if there is a button in form, it will close the modal */}
-            <button className="btn">Close</button>
+        <div className="flex flex-col justify-center mt-0 modal-action">
+          {/*start form section  */}
+          <form className="card-body" method="dialog">
+            <h3 className="text-lg font-bold">Please Login!</h3>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+              />
+              <label className="mt-1 label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            {/*start error txt */}
+
+            {/*end error txt */}
+
+            {/*start login button*/}
+            <div className="mt-6 form-control">
+              <input
+                type="submit"
+                value="Login"
+                className="btn bg-[#FF9800] text-white"
+              />
+            </div>
+            {/*end login button*/}
+            {/*start signup txt */}
+            <p className="my-2 text-center">
+              Do not have an account ?{" "}
+              <Link to="/signup" className="ml-1 underline text-red">
+                signup now
+              </Link>
+              {""}
+            </p>
+            {/*end sign up txt */}
+            {/*start close btn */}
+            <button
+              htmlFor="my_modal_5"
+              onClick={() => document.getElementById("my_modal_5").close()}
+              className="absolute btn btn-sm btn-circle btn-ghost right-2 top-2"
+            >
+              ✕
+            </button>
+            {/*end close btn*/}
           </form>
+          {/*end Form section */}
+
+          {/*start log with social media*/}
+          <div className="mb-5 space-x-5 text-center">
+            <button className="btn btn-circle hover:bg-orange">
+              <FaGoogle />
+            </button>
+            <button className="btn btn-circle hover:bg-orange">
+              <FaFacebookF />
+            </button>
+            <button className="btn btn-circle hover:bg-orange">
+              <FaGithub />
+            </button>
+          </div>
+          {/*end log with social media*/}
         </div>
       </div>
     </dialog>
